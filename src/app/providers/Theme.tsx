d9140ffb0f36cce3bc8d14ed6e86e3184f8e5a19
Sanich <sancho@cdrpro.ru>
2019-05-23 18:36:34 +0300

    Added prefers-color-scheme support

--- src/app/providers/Theme.tsx
+++ src/app/providers/Theme.tsx
@@ -1,16 +1,24 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useLayoutEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import ThemeContext from '../contexts/ThemeContext';
 import setTheme from '../helpers/setTheme';
 import { uiThemeName } from '../types';
 
+const DEFAULT_THEME =
+  matchMedia('(prefers-color-scheme: dark)').matches
+    ? uiThemeName.Dark
+    : uiThemeName.Light;
+
 const Theme: React.FunctionComponent = observer(({ children }) => {
-  const [theme, updateTheme] = useState(uiThemeName.Light);
+  const [theme, updateTheme] = useState(DEFAULT_THEME);
 
   const onUpdateTheme = useCallback((theme: uiThemeName) => {
     updateTheme(theme);
-    setTheme(theme);
   }, []);
+
+  useLayoutEffect(() => {
+    setTheme(theme);
+  }, [theme]);
 
   return (
     <ThemeContext.Provider value={[theme, onUpdateTheme]}>
